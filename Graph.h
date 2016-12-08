e7c4badfd6adeb65bd29bf2c43419625ba7910a6
Xuan Do <xdo0410@gmail.com>
2016-12-07 21:51:01 -0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -6,19 +6,28 @@ public:
 	// TO DO
 	// initialize an undirected graph that can store at most n vertices
 	Graph(const int n) {
-
+		this -> numberVertices = n;
+		adjVector = new std::vector<int>[numberVertices];
+		for (int i = 0; i < numberVertices; i++)
+			for (int j = 0; j< numberVertices; j++){
+			adjVector[i][j] = false;
+		}
 	}
 
 	// TO DO
 	// insert an edge between vertices u and v
 	void insertEdge(int u, int v) {
-
+		adjVector[u].push_back(v);
 	}
 
 	// TO DO
 	// return the set of vertices that are adjacent to v
 	std::vector<int> adjacentVertices(int v) {
 		std::vector<int> vertices;
+		for (int j = 0; j < capacity; j++) {
+                     if([v][j] == true)
+			     verticies.push(j+1);
+		  }
 
 		return vertices;
 	}
@@ -35,5 +44,6 @@ public:
 private:
 	// TO DO
 	// member variables and functions to implement the public member functions
-
+	int numberVertices;
+	std::vector<int> *adjVector;
 };
