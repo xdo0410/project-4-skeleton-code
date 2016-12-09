e4390243596045ff5805f3885dd37562d4673983
Xuan Do <xdo0410@gmail.com>
2016-12-09 13:36:01 -0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <vector>
 
+using namespace std;
 
 class Graph {
 public:
 	// TO DO
 	// initialize an undirected graph that can store at most n vertices
-	Graph() {}
+	//Graph(const int n) {
+	Graph() {
+		//this->numberVertices = n;
+		//adjVector = new std::vector<int>[numberVertices];
+
+	}
 
 	// TO DO
 	// insert an edge between vertices u and v
 	void insertEdge(int u, int v) {
+		// *** Check if u is within range, if not then resize the vector to size u.
 		if (u >= adjVector.size()) {
 			adjVector.resize(u+1);
 		}
@@ -24,40 +31,57 @@ public:
 	// TO DO
 	// return the set of vertices that are adjacent to v
 	std::vector<int> adjacentVertices(int v) {
-		std::vector<int> vertices;
-		for (int j = 0; j < numberVertices; j++) {
-            		if(adjVector[v][j] == true)
-			vertices.push_back(j+1);
-		  }
+		if ((v < 1) || (v > adjVector.size()))
+			throw invalid_argument("The param v is out of range.");
 
-		return vertices;
+		return adjVector[v];
 	}
 
 	// TO DO
 	// return a list of vertices that appear between v and w, starting with v and ending with w
 	// vertices should not be repeated
-	
 	std::vector<int> getPath(int v, int w) {
-		std::vector<int> getPath(int v, int w) {
+		if ((v < 1) || (v > adjVector.size()))
+			throw invalid_argument("The param v is out of range.");
+
+		return buildPathTrail(v, w);
+	}
+
+private:
+	// TO DO
+	// member variables and functions to implement the public member functions
+	int numberVertices;
+	//std::vector<int> *adjVector;
+	std::vector<std::vector<int>> adjVector;
+
+	vector<bool> isVisited;
+	vector<int> pathTrail;
+
+	vector<int> buildPathTrail(int startingSource, int target) {
+		// Initially, mark all the source vertices as not yet visited.
 		isVisited.resize(adjVector.size());
 		for (int i = 0; i < adjVector.size(); i++) {
 			isVisited[i] = false;
 		} // for
 
-		path.clear();
-
-		bool isFoundPath = getToTarget(v, w);
+		// Reset pathTrail in case it was built from previously iteration.
+		pathTrail.clear();
+		bool isFoundPath = getToTarget(startingSource, target);
 		if (isFoundPath)
-			path.push_back(v);
+			pathTrail.push_back(startingSource);
+		// TODO: maybe throw an exception if path is not found y checking the returned isFoundPath param.
 
-
-		std::vector<int> pathTrailToReturn;
-		for (auto rit = path.crbegin(); rit != path.crend(); ++rit)
+		// The path is built in reverse order, i.e. target back to starting point, because the nature of using vector<int>.
+		// We just need to reverse it for the program caller to work naturally.
+		vector<int> pathTrailToReturn;
+		for (auto rit = pathTrail.crbegin(); rit != pathTrail.crend(); ++rit)
 			pathTrailToReturn.push_back(*rit);
 
 		return pathTrailToReturn;
 	}
 
+	// This function resursively trying to find the first path by travel from the source to target by visiting 
+	// every adjacent nodes and their adjacent ones until hitting the target.
 	bool getToTarget(int source, int target) {
 		bool targetFound = false;
 		isVisited[source] = true;
@@ -72,7 +96,7 @@ public:
 					if (targetFound) {
 						// Only add to the trail path if not already there.
 						if (!isAlreadyInTrailPath(nextSourceToVisit))
-							path.push_back(nextSourceToVisit);
+							pathTrail.push_back(nextSourceToVisit);
 						break; // Found the path to target so no need to continue. Exit immediately.
 					} // if						
 				} // if
@@ -82,22 +106,11 @@ public:
 	}
 
 	bool isAlreadyInTrailPath(int source) {
-		for (int i = 0; i < path.size(); i++) {
-			if (path[i] == source)
+		for (int i = 0; i < pathTrail.size(); i++) {
+			if (pathTrail[i] == source)
 				return true;
 		} // for
 		return false;
 	}
-	
-
-
-private:
-	// TO DO
-	// member variables and functions to implement the public member functions
-	int numberVertices;
-	std::vector<std::vector<int>> adjVector;
-	
-	std::vector<bool> isVisited;
-	std::vector<int> path;
-	
 };
+
