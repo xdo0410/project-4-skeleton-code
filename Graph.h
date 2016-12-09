385ae3fcef55362e5064549729b94a8fdae21779
Xuan Do <xdo0410@gmail.com>
2016-12-08 23:34:18 -0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -34,28 +34,53 @@ public:
 	// return a list of vertices that appear between v and w, starting with v and ending with w
 	// vertices should not be repeated
 	
-	void dfs(int s) {
-		std::vector <bool> visited;
-		visited[s] = true;
-		for (int i = 0; i < adjVector[s].size(); i++)
-			if (!visited[adjVector[s][i]])
-				dfs(adjVector[s][i]);
-	}
-	
-	std::vector<int> getPath(int v, int w) {
+		std::vector<int> getPath(int w) {
 		std::vector<int> path;
-		for (int i =0; i < adjVector[v].size(); i++) {
-			for (int j =0; j < adjVector[w].size(); j++){
-				if(adjVector[v][i] == adjVector[w][j])
-				path.push_back(adjVector[v][i]);
-			}
+		int cur = w;
+		while(cur!= -1){
+			path.push_back(cur);
+			cur = from[cur];	
 		}
+
 		return path;
 	}
+
+	int BFS(int v, int w){ // source and destination, returns the length of the shortest path between src and dest if reachable, otherwise returns -1
+		std::vector <bool> visited;
+		
+		queue<int>q;
+		int level=0,nextLevelNodes=0,curLevelNodes=1;
+		q.push(v); visited[v]=true;
+
+		while(!q.empty()){
+			int cur = q.front(); q.pop();
+			curLevelNodes--;
+			for(int i = 0 ; i < adjVector[cur].size();i++){
+				if(!visited[adjVector[cur][i]]){
+				q.push(adjVector[cur][i]);
+				from[adjVector[cur][i]]=cur;
+				visited[adjVector[cur][i]]=true;
+				nextLevelNodes++;
+				if(adjVector[cur][i]==w) 
+					return level+1;
+       }
+     }
+
+			if(!curLevelNodes){
+			curLevelNodes=nextLevelNodes;
+			nextLevelNodes=0;
+			level++;
+			}
+		}
+		return -1;
+	}
+
+	
 
 private:
 	// TO DO
 	// member variables and functions to implement the public member functions
 	int numberVertices;
 	std::vector<int> *adjVector;
+	std::vector<int> from;
 };
