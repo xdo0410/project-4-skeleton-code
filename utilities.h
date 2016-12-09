3d1e68edcf525aec3152e5a88916fc432658b376
Xuan Do <xdo0410@gmail.com>
2016-12-09 12:39:06 -0800

    Update utilities.h

--- utilities.h
+++ utilities.h
@@ -22,15 +22,21 @@ std::vector<string> readStationNames(const std::string& filename) {
 	int id;
 	string station;
 	std::vector<string>station_names;
-	
-	while(fin)
+	std::vector<string>::iterator it;
+
+	while (fin)
 	{
 		fin >> station >> id;
-		station_names.push_back(station);
+
+		if (id >= station_names.size())
+			station_names.resize(id+1);
+
+		//it = station_names.begin();
+		station_names[id] = station;
+		//station_names.push_back(station);
 	}
-	fin.close();
-	
-	return std::vector<string>();
+
+	return station_names;
 }
 
 // TO DO
@@ -43,8 +49,16 @@ Graph *buildGraphfromDatafile(const std::string& filename) {
 	
 	if(!fin.is_open())
 		throw invalid_argument("Unable to open file " + filename);
-	Graph *station;
-	fin.close();
 	
-	return NULL;
+	int station, adjacentStation;
+	Graph *stationGraph = new Graph();
+
+	while (fin)
+	{
+		fin >> station >> adjacentStation;
+		stationGraph->insertEdge(station, adjacentStation);
+	} // while
+
+	return stationGraph;
+	//return NULL;
 }
